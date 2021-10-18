34d28070398c5c56ac8bb96e46f98735176abf68
Tomas Pesek <pesekt@gmail.com>
2021-10-18 22:26:50 +0200

    navbar count

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,10 @@ class Navbar extends Component {
         <nav className="navbar navbar-light bg-light">
           <div className="container-fluid">
             <a className="navbar-brand" href="#">
-              Navbar
+              Navbar:{" "}
+              <span className="badge rounded-pill bg-primary">
+                {this.props.totalCounters}
+              </span>
             </a>
           </div>
         </nav>
