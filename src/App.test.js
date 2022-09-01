ccf4706fdd87568b7f3f2384d120738b3d52a554
Tomas Pesek <pesekt@gmail.com>
2022-09-01 12:58:52 +0200

    test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders Navbar text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Navbar/i);
+  const linkElement = screen.getByText(/something/i);
   expect(linkElement).toBeInTheDocument();
 });
